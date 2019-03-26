0700b5f37367b976fd28cb736e18cc7c5dcff5e0
¨jscavuzzo¨ <juan.scavuzzo@mercadolibre.com>
2019-03-26 11:32:54 -0300

    Add rompy repo

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+
+setup(
+    name='rompy',
+    version=0.1,
+    description='Python package for reduced-order modeling applications',
+    url='https://bitbucket.org/chadgalley/rompy.git',
+    packages=find_packages(),
+)
